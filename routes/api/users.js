918938e4ffa98943019322de721aa9c61a56c059
nickpape94 <nick-_p@hotmail.com>
2019-12-01 17:41:51 +0000

    JWT token for user added

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -48,8 +50,16 @@ router.route('/').post([
 		user.password = await bcrypt.hash(password, salt);
 
 		await user.save();
-		// Return jsonwebtoken
-		res.send('User registered');
+		// Json web token for authentication
+		const payload = {
+			user: {
+				id: user.id
+			}
+		};
+		jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600000 }, (err, token) => {
+			if (err) throw err;
+			res.json({ token });
+		});
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server error');
