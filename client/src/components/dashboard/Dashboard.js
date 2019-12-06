8a0009be4fb3bbf6f91cb984c9729b475d2b5a21
nickpape94 <nick-_p@hotmail.com>
2019-12-06 17:06:37 +0000

    List education & experiences

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,12 +4,17 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
-	useEffect(() => {
-		getCurrentProfile();
-	}, []);
+	useEffect(
+		() => {
+			getCurrentProfile();
+		},
+		[ getCurrentProfile ]
+	);
 
 	return loading && profile === null ? (
 		<Spinner />
@@ -22,6 +27,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 			{profile !== null ? (
 				<Fragment>
 					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
 				</Fragment>
 			) : (
 				<Fragment>
