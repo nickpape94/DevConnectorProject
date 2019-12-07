09c4795ac792621b0120d5e7b8727c0ce90918c2
nickpape94 <nick-_p@hotmail.com>
2019-12-07 12:23:29 +0000

    Finished dashboard managment system

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -69,7 +69,7 @@ const AddEducation = ({ addEducation, history }) => {
 					<input type='date' name='from' value={from} onChange={(e) => onChange(e)} />
 				</div>
 				<div className='form-group'>
-					<p>
+					{/* <p>
 						<input
 							type='checkbox'
 							name='current'
@@ -80,8 +80,8 @@ const AddEducation = ({ addEducation, history }) => {
 								toggleDisabled(!toDateDisabled);
 							}}
 						/>{' '}
-						Current Job
-					</p>
+						Current School
+					</p> */}
 				</div>
 				<div className='form-group'>
 					<h4>To Date</h4>
