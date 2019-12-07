8652c3210cb7b4c5518a8a4580e847b242264ac2
nickpape94 <nick-_p@hotmail.com>
2019-12-07 14:18:57 +0000

    Display profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -31,6 +31,7 @@ export const getCurrentProfile = () => async (dispatch) => {
 // Get all profiles
 export const getProfiles = () => async (dispatch) => {
 	dispatch({ type: CLEAR_PROFILE });
+
 	try {
 		const res = await axios.get('/api/profile');
 
@@ -41,7 +42,7 @@ export const getProfiles = () => async (dispatch) => {
 	} catch (err) {
 		dispatch({
 			type: PROFILE_ERROR,
-			payload: { msg: err.response.statusText, status: err.response.status }
+			payload: { msg: err.response.statusText, status: err.response.status } // change this.
 		});
 	}
 };
