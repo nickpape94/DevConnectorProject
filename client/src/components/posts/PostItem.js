6e089dcdeca996c7e3bb7a8429a2fb234283f9d4
nickpape94 <nick-_p@hotmail.com>
2019-12-08 15:39:51 +0000

    Add a post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -15,17 +15,13 @@ const PostItem = ({
 	return (
 		<div className='post bg-white p-1 my-1'>
 			<div>
-				<a href='profile.html'>
+				<Link to={`/profile/${user}`}>
 					<img className='round-img' src={avatar} alt='' />
 					<h4>{name}</h4>
-				</a>
+				</Link>
 			</div>
 			<div>
-				<p className='my-1'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus corporis sunt necessitatibus!
-					Minus nesciunt soluta suscipit nobis. Amet accusamus distinctio cupiditate blanditiis dolor? Illo
-					perferendis eveniet cum cupiditate aliquam?
-				</p>
+				<p className='my-1'>{text}</p>
 				<p className='post-date'>
 					Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
 				</p>
